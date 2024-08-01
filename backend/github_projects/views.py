4b9ccce3916d95b697942506d14f13649a768796
hietbrinkluan <158548906+hietbrinkluan@users.noreply.github.com>
2024-08-01 14:24:50 +0200

    Figuring out web scrapers

--- backend/github_projects/views.py
+++ backend/github_projects/views.py
@@ -1,9 +1,17 @@
 from rest_framework import viewsets
 from .models import Project
 from .serializers import ProjectSerializer
+from bs4 import BeautifulSoup
+import requests
 
 class ProjectViewSet(viewsets.ModelViewSet):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 
+def scrape_athlete_results(athletecountry, athletename=None, athletesurname=None, athletenumber=None):
+    url = f"https://worldathletics.org/athletes/{athletecountry}/{athletename}-{athletesurname}-{athletenumber}"
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text,)
+
+    return None
